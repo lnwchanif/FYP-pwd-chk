81521b9bd1c1b2bed659dcfcaa925e59ab9e374d
lnwChanif <148457594+lnwchanif@users.noreply.github.com>
2025-05-07 20:01:50 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -93,16 +93,17 @@ document.getElementById('password').addEventListener('input', async function() {
                 }
             }
             if (breached) {
-                const exclamationIcon  = `
-                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
+             const exclamationIcon = `
+                <svg xmlns="http://www.w3.org/2000/svg" class = "warningIcon" width="40" height="40" fill="currentColor" class="bi bi-exclamation-circle" viewBox="0 0 16 16">
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                 <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
                 </svg>`;
-                HIBP_feedback.innerHTML = exclamationIcon  + "Your password has been breached " + breachCount + " times.";
+                HIBP_feedback.innerHTML = exclamationIcon + "<b>Your password has been breached </b>" + breachCount + "<b> times.</b>";
                 HIBP_feedback.style.color = "red";
             } else {
-                HIBP_feedback.textContent = "Your password does not appear in leaked passwords";
-                HIBP_feedback.style.color = "green";
+                const checkIcon = 
+                '<svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" class = "warningIcon" fill="currentColor" class="bi bi-check-circle" viewBox="0 0 16 16"> <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/> <path d="m10.97 4.97-.02.022-3.473 4.425-2.093-2.094a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-1.071-1.05"/></svg>';
+                HIBP_feedback.innerHTML = checkIcon + "<b>Your password does not appear in leaked passwords</b>";                HIBP_feedback.style.color = "green";
             }
         } catch (error) {
             console.error("Error checking breach status:", error);
