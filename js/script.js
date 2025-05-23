085d8a8bfce662491eb0ab2d4d7213b2303ef751
lnwChanif <148457594+lnwchanif@users.noreply.github.com>
2025-05-23 14:54:00 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -122,7 +122,7 @@ document.getElementById('password').addEventListener('input', async function() {
                 <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16"/>
                 <path d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0M7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0z"/>
                 </svg>`;
-                HIBP_feedback.innerHTML = exclamationIcon + "<b>Your password has been breached </b>" + breachCount.toLocalString('en') + "<b> times.</b>";
+                HIBP_feedback.innerHTML = exclamationIcon + "<b>Your password has been breached </b>" + breachCount.toLocaleString('en') + "<b> times.</b>";
                 HIBP_feedback.style.color = "red";
             } else {
                 const checkIcon = 
